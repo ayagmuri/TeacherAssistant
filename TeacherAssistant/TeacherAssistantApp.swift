dc78e2934dfd03458133725e5c6042360dc33e98
Amin Yaghmouri <amin.yaghmouri@gmail.com>
2025-04-07 14:22:38 -0700

    Initial Commit

---
+++ TeacherAssistant/TeacherAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherAssistantApp.swift
+//  TeacherAssistant
+//
+//  Created by Amin Yaghmouri on 2025-04-07.
+//
+
+import SwiftUI
+
+@main
+struct TeacherAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
